e80a97275c292a06bd9ac9dc4d0f7b7bf90362ad
AnastasiiaKotielnikova <akotelnikova1993@gmail.com>
2022-09-28 10:19:20 +0300

    add scss

--- src/js/refs.js
+++ src/js/refs.js
@@ -1,5 +1,5 @@
 const refs = {
-  teamContainer: document.querySelector('.eam-container'),
+  teamContainer: document.querySelector('.team-container'),
   openModalBtn: document.querySelector('[data-modal-team-open]'),
   closeModalBtn: document.querySelector('[data-modal-team-close]'),
   teamModal: document.querySelector('[data-modal-team]'),
