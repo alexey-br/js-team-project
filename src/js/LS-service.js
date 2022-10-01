6a19ed57aaa70fbc0d0dcb34a32a03e119d33b1d
Alexey Brazhnik <alexey.brazhnyk@gmail.com>
2022-10-01 11:13:55 +0300

    попытка вылечить модалку

--- src/js/LS-service.js
+++ src/js/LS-service.js
@@ -1,28 +1,28 @@
-export function addMovieToList(movieData, listName) {
-  const prevList = JSON.parse(localStorage.getItem(listName));
-  let newList = [];
+// export function addMovieToList(movieData, listName) {
+//   const prevList = JSON.parse(localStorage.getItem(listName));
+//   let newList = [];
 
-  if (prevList) {
-    newList = [...prevList, movieData];
-  } else {
-    newList = [movieData];
-  }
+//   if (prevList) {
+//     newList = [...prevList, movieData];
+//   } else {
+//     newList = [movieData];
+//   }
 
-  localStorage.setItem(listName, JSON.stringify(newList));
-}
+//   localStorage.setItem(listName, JSON.stringify(newList));
+// }
 
-export function removeMovieFromList(idToRemove, listName) {
-  const prevList = JSON.parse(localStorage.getItem(listName));
-  if (!prevList) return;
+// export function removeMovieFromList(idToRemove, listName) {
+//   const prevList = JSON.parse(localStorage.getItem(listName));
+//   if (!prevList) return;
 
-  const newList = prevList.filter(({ id }) => id !== idToRemove);
+//   const newList = prevList.filter(({ id }) => id !== idToRemove);
 
-  localStorage.setItem(listName, JSON.stringify(newList));
-}
+//   localStorage.setItem(listName, JSON.stringify(newList));
+// }
 
-export function checkMovieIsInList(movieId, listName) {
-  console.log(movieId);
-  const moviesList = JSON.parse(localStorage.getItem(listName));
-  const movieIsInList = moviesList.some(({ id }) => id === movieId);
-  return movieIsInList;
-}
+// export function checkMovieIsInList(movieId, listName) {
+//   console.log(movieId);
+//   const moviesList = JSON.parse(localStorage.getItem(listName));
+//   const movieIsInList = moviesList.some(({ id }) => id === movieId);
+//   return movieIsInList;
+// }
